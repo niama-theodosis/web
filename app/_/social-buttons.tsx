259997c607b096d797ab2f97d4200716c7d7b82a
Gregory Bouteiller <gregory.bouteiller@gmail.com>
2025-05-22 10:49:52 +0400

    up

--- app/_/social-buttons.tsx
+++ app/_/social-buttons.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 import { cn } from "@/lib/utils";
 import { Facebook, Instagram, Youtube } from "lucide-react";
 import type { HTMLAttributes, Ref } from "react";
@@ -6,19 +7,28 @@ import type { HTMLAttributes, Ref } from "react";
 // ROOT ************************************************************************************************************************************
 export const SocialButtons = ({ className, ...props }: SocialButtonsProps) => {
   const socials = [
-    { id: "instagram", Icon: Instagram, url: "https://www.instagram.com/theodosis_memoirescellulaires" },
-    { id: "youtube", Icon: Youtube, url: "https://www.youtube.com/@Theod%C3%B4sisM%C3%A9moiresCellulairesTV/videos" },
-    { id: "facebook", Icon: Facebook, url: "https://www.facebook.com/TheodosisMemoiresCellulaires" },
+    { id: "Instagram", Icon: Instagram, url: "https://www.instagram.com/theodosis_memoirescellulaires" },
+    { id: "Youtube", Icon: Youtube, url: "https://www.youtube.com/@Theod%C3%B4sisM%C3%A9moiresCellulairesTV/videos" },
+    { id: "Facebook", Icon: Facebook, url: "https://www.facebook.com/TheodosisMemoiresCellulaires" },
   ];
 
   return (
     <div className={cn("flex items-center gap-1 lg:gap-2", className)} {...props}>
       {socials.map(({ Icon, id, url }) => (
-        <Button key={id} size="icon" color="primary" asChild>
-          <a href={url ?? ""} target="_blank" aria-label={id}>
-            <Icon className="h-4 w-4"></Icon>
-          </a>
-        </Button>
+        <TooltipProvider key={id}>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button size="icon" color="primary" asChild>
+                <a href={url ?? ""} target="_blank" aria-label={id}>
+                  <Icon className="h-4 w-4"></Icon>
+                </a>
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>{id}</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
       ))}
     </div>
   );
